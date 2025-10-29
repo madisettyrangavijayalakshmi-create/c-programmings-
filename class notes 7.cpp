410a973359c4edcc517b99bf6c69b74d43f710e1
madisettyrangavijayalakshmi-create <madisettyrangavijayalakshmi@gmail.com>
2025-10-29 10:13:57 +0530

    Add files via upload

---
+++ class notes 7.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter  the values of a,b,c");
+	scanf("%d%d%d",&a,&b,&c);
+	sum=a+b+c;
+	avg=sum/5;
+	printf("sum=%d",sum);
+	printf("avg=%d",avg);
+	return 0;	
+}
