2a22358af865d989cc66532c1648a163e6ec1f73
madisettyrangavijayalakshmi-create <madisettyrangavijayalakshmi@gmail.com>
2025-11-16 11:36:12 +0530

    Add files via upload

---
+++ cat with string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char s1[20],s2[20];
+	printf("enter string 1 name");
+	gets(s1);
+	printf("enter string 2 name");
+	gets(s2);
+	strcat(s1,s2);
+	printf("merge string=%s",s1);
+	return 0;
+}
