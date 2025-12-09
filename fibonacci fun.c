5cd2b506917e3b6cb1527de5d081eea14e9ffc4f
madisettyrangavijayalakshmi-create <madisettyrangavijayalakshmi@gmail.com>
2025-12-09 17:04:03 +0530

    Add files via upload

---
+++ fibonacci fun.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void fib(int);
+int main()
+{
+	int n;
+	printf("enter array size");
+	scanf("%d",&n);
+	fib(n);
+	return 0;
+}
+void fib(int n)
+{
+	int f=0,s=1,t;
+	printf("%d%d",f,s);
+	t=f+s;
+	while(t<=n){
+		printf("%d",t);
+		f=s;
+		s=t;
+		t=f+s;
+	}
+}
